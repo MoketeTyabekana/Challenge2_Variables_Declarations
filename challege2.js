4d3021e59aea7419c014524296038420f224af08
Mokete Elias Tyabekana <75148610+MoketeTyabekana@users.noreply.github.com>
2024-05-23 14:28:16 +0200

    Declared a Bolean, String, first number, second number and re-assigned a new value to the second number. Then Declared an Array that takes two strings.

--- challege2.js
+++ challege2.js
@@ -5,5 +5,23 @@
 let myBool=true;
 console.log(myBool);
 
+//String Variable
+let myString='Hello World';
+console.log(myString);
+
+//Number variable
+let firstNumber=20;
+console.log(firstNumber);
+
+let secondNumber=40;
+console.log(secondNumber);
+
+//Re-Assigning the secondNumber to a new value.
+secondNumber=80;
+console.log(secondNumber);
+
+//Array
+let myArray=['myBolean','myString'];
+console.log(myArray[0]);
 
 
